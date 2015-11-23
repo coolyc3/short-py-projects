64bdf900ffbc87be90e7f05689b779a5e29ccac3
coolyc3 <coolyc3@gmail.com>
2015-11-23 17:54:47 -0500

    Update slope_intercept.py

--- slope_intercept.py
+++ slope_intercept.py
@@ -1,4 +1,4 @@
-ifslope = raw_input("Do you have a slope value?(y or n)")
+fslope = raw_input("Do you have a slope value?(y or n)")
 if ifslope == "y":
     slope2 = raw_input("Type slope part one. (etc. -->1/x)")
     slope1 = raw_input("Type slope part two. (etc. y/1<--)")
