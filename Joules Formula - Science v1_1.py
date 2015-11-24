529078ff00bec4e548f46286dc03e836ca007c7f
Yonaton Chriqui <coolyc3@gmail.com>
2015-11-24 15:24:26 -0500

    Update Joules Formula - Science v1_1.py

--- Joules Formula - Science v1_1.py
+++ Joules Formula - Science v1_1.py
@@ -23,36 +23,30 @@ if num == 1:
     z = float(raw_input("Type your Specific Heat"))
     print heat(x,y,z)
 elif num == 2:
-    x = raw_input("Type your Heat")
-    y = raw_input("Type your Specific Heat")
-    z = raw_input("Type your Mass")
+    x = float(raw_input("Type your Heat"))
+    y = float(raw_input("Type your Specific Heat"))
+    z = float(raw_input("Type your Mass"))
     print rest(x,y,z)
 elif num == 3:
-    x = raw_input("Type your Heat")
-    y = raw_input("Type your Mass")
-    ask = raw_input("Do you have Change in Tempurature. 1 for Yes. 2 for No.")
-    ask = int(ask)
+    x = float(raw_input("Type your Heat"))
+    y = float(raw_input("Type your Mass"))
+    ask = int(raw_input("Do you have Change in Tempurature. 1 for Yes. 2 for No."))
     if ask == 1:
-        z = raw_input("Type your Change in Tempurature")
+        z = float(raw_input("Type your Change in Tempurature"))
     else:
-        temp1 = raw_input("Type temp1 (temp1 > temp2)")
-        temp2 = raw_input("Type temp2")
-        temp1 = int(temp1)
-        temp2 = int(temp2)
+        temp1 = float(raw_input("Type temp1 (temp1 > temp2)"))
+        temp2 = float(raw_input("Type temp2"))
         z = temp1 - temp2
     print rest(x,y,z)
 elif num == 4:
-    x = raw_input("Type your Heat")
-    y = raw_input("Type your Specific Heat")
-    ask = raw_input("Do you have Change in Tempurature. 1 for Yes. 2 for No.")
-    ask = int(ask)
+    x = float(raw_input("Type your Heat"))
+    y = float(raw_input("Type your Specific Heat"))
+    ask = int(raw_input("Do you have Change in Tempurature. 1 for Yes. 2 for No."))
     if ask == 1:
-        z = raw_input("Type your Change in Tempurature")
+        z = float(raw_input("Type your Change in Tempurature"))
     else:
-        temp1 = raw_input("Type temp1 (temp1 > temp2)")
-        temp2 = raw_input("Type temp2")
-        temp1 = int(temp1)
-        temp2 = int(temp2)
+        temp1 = float(raw_input("Type temp1 (temp1 > temp2)"))
+        temp2 = float(raw_input("Type temp2"))
         z = temp1 - temp2
     print rest(x,y,z)
 quit = raw_input("Press enter to quit")
