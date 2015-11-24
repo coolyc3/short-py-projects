61c1930eed5617ae3ffa50c0559c444fca41ec8c
Yonaton Chriqui <coolyc3@gmail.com>
2015-11-23 21:39:53 -0500

    Add 99 bottles of beer

---
+++ 99 Bottles of beer.py
@@ -0,0 +1,12 @@
+x = 99
+while x > 0:
+    if x == 2:
+    	print x,"bottles of beer on the wall",x,"bottles of beer! Take one down pass it around,",x-1,"bottle of beer on the wall!"
+    	x -= 1
+    elif x == 1:
+    	print x,"bottle of beer on the wall",x,"bottle of beer! Take one down pass it around, No more bottles of beer on the wall!"
+    	x -= 1
+    elif x != 1:
+		print x,"bottles of beer on the wall",x,"bottles of beer! Take one down pass it around,", x-1,"bottles of beer on the wall!"
+    	x -= 1
+quit = raw_input("Press enter to quit")
